5a3e8f2731762f97ff5aa234d28962c93b0937d9
04zAM <49489566+04zAM@users.noreply.github.com>
2022-01-19 12:13:28 -0500

    connecting CRUDs to REST services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PizzaComponent } from './components/pizza/pizza.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { IngredientsComponent } from './components/ingredients/ingredients.component';
+import { PizzaIngredientsComponent } from './components/pizza-ingredients/pizza-ingredients.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaComponent
+    PizzaComponent,
+    IngredientsComponent,
+    PizzaIngredientsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
