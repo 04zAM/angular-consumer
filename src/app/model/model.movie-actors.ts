bb2020373eb29a23ce589d756b0a1b46478f176f
04zAM <49489566+04zAM@users.noreply.github.com>
2022-01-24 17:33:58 -0500

    Auto-generated commit [ci skip]

--- src/app/model/model.movie-actors.ts
+++
@@ -1,8 +0,0 @@
-export interface ModelMovieActors {
-  act_mov_id: number;
-  mov_id: number;
-  act_id: number;
-  act_mov_state?: boolean;
-  act_mov_actor_principal?: boolean;
-  act_name?: string;
-}
